6b107fe6f6f12eaa59e72bf4888a468a67044121
Munteanu Bogdan <b.muntanu@dextechnology.com>
2026-05-23 19:48:30 +0300

    initial app

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Моё мини-приложение MAX</h1>
+      <button onClick={() => alert('Привет!')}>
+        Нажми меня
+      </button>
+    </main>
+  );
+}
